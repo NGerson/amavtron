5d0ca71f246fba24b315024f3322e4c6f09b9c9c
NGerson <87144299+NGerson@users.noreply.github.com>
2024-04-19 17:30:49 -0300

    change js

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,3 @@
-/**
- * add event listener on multiple elements
- */
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
